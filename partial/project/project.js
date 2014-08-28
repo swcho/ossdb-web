3f274fb8505e322db6b27a69748bb0564da675a6
sungwoo <swforgemaster@gmail.com>
2014-08-28 14:32:36 +0900

    Project list

--- partial/project/project.js
+++ partial/project/project.js
@@ -1,4 +1,7 @@
-angular.module('ossdbWeb').controller('ProjectCtrl',function($scope){
+angular.module('ossdbWeb').controller('ProjectCtrl',['$scope', '$ossdb', function($scope, $ossdb){
 
+    $ossdb.getProjectList(function (projectList) {
+        $scope.projectList = projectList
+    });
 
-});
+}]);
--- service/ossdb.js
+++ service/ossdb.js
@@ -1,6 +1,18 @@
-angular.module('ossdbWeb').factory('ossdb',function() {
+angular.module('ossdbWeb').factory('$ossdb',['$http', function($http) {
 
-	var ossdb = {};
+
+    var port = 1337;
+
+    var baseUrl = 'http://' + window.location.hostname + ':' + port
+
+	var ossdb = {
+        getProjectList: function(cb) {
+            console.log('ossdb.getProjectList');
+            $http.get(baseUrl + '/project').success(function(project) {
+                cb(project);
+            });
+        }
+    };
 
 	return ossdb;
-});
+}]);
